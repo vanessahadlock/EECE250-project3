34a00839d92202c6a09e7e52b551625209be75ac
vhadlock <32147859+vhadlock@users.noreply.github.com>
2020-06-08 17:01:32 -0400

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -12,27 +12,22 @@ Grid::Grid() {
     ifstream inFile("input15.txt"); //opens the file of letters
 
     if (!inFile) {
-    
+        
         cout << "Unable to open file the letter text file";
         exit(1);   // call system to stop
     }
 
+//read the first number in the first line in order to get the size of the grid
 //    int sLine;
 
 //    n >> sLine;
-
-//    const size_t n = 15;         //set n equal to the dimensions of the matrix (grid) 
-  
-//    char grid[ n ][ n ] = {};    //create a matrix characters called grid of size n x n
     
-    int column = 0;     //initialize column to 0 to start
-    int row = 0;        //initialize row to 0 to start
+    column = 0;     //initialize column to 0 to start
+    row = 0;        //initialize row to 0 to start
   
     inFile >> column >> row;
 
-    const int n = column; 
-
-    char grid[ n ][ n ] = {};
+    grid[ n ][ n ] = {};        //create a matrix characters called grid of size n x n
 
     for(int column_counter = 1; column_counter < column; column_counter++) {
 
@@ -42,19 +37,21 @@ Grid::Grid() {
 
         }
     }
-  
+    
     inFile.close();
+}
+
+
+void Grid::printGrid(const int size) {
   
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < size; i++) {
       
-        for(int j = 0; j < n; j++) {
+        for(int j = 0; j < size; j++) {
           
         cout<< grid[i][j] << " ";
         
         }
         cout<<endl;
     }
-
     cout<<endl;
-
 }
